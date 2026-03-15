83f9689cc3b7fe20c817bc6cc835c99dda391354
filatov.v.k <filatov.v.k@mail.ru>
2026-03-15 10:54:30 +0000

    Update

--- 18-02/variables.tf
+++ 18-02/variables.tf
@@ -1,27 +1,27 @@
 ###cloud vars
 
-
 variable "cloud_id" {
   type = string
   default = "b1goo5pkjq9ldvqkgp0l"
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id  "
 }
 
 variable "folder_id" {
   type = string
   default = "b1g7c66oo5q6sjc4bdlt"
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id  "
 }
 
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
-  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope  "
 }
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create  "
 }
 
 variable "vpc_name" {
@@ -30,6 +30,61 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+### VM Web Variables (prefix: vm_web_)
+
+variable "vm_web_name" {
+  type        = string
+  description = "Name of the web virtual machine"
+  default     = "netology-develop-platform-web"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  description = "Platform type for web VM"
+  default     = "standard-v3"
+}
+
+variable "vm_web_cores" {
+  type        = number
+  description = "Number of CPU cores for web VM"
+  default     = 2
+}
+
+variable "vm_web_memory" {
+  type        = number
+  description = "RAM size in GB for web VM"
+  default     = 1
+}
+
+variable "vm_web_core_fraction" {
+  type        = number
+  description = "Core fraction percentage for web VM"
+  default     = 20
+}
+
+variable "vm_web_image_family" {
+  type        = string
+  description = "Image family for web VM"
+  default     = "ubuntu-2004-lts"
+}
+
+variable "vm_web_preemptible" {
+  type        = bool
+  description = "Preemptible mode for web VM"
+  default     = true
+}
+
+variable "vm_web_nat" {
+  type        = bool
+  description = "Assign NAT IP address to web VM"
+  default     = true
+}
+
+variable "vm_web_serial_port_enable" {
+  type        = number
+  description = "Enable serial console for web VM (1 = enabled)"
+  default     = 1
+}
 
 ###ssh vars
 
