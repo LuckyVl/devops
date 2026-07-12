d3b940fb6dea7d93aa5f050f67435d7ea3eabd5c
filatov.v.k <filatov.v.k@mail.ru>
2026-07-12 11:43:24 +0000

    update 23-01

--- 23-01/README.md
+++ 23-01/README.md
@@ -5,3 +5,6 @@ cd ~/IdeaProjects/devops/23-01
 ## Ручное получение параметров подключения (в k8s прописано авто получение)
 yc managed-kubernetes cluster get-credentials --id <введи номер куба> --external
 
+
+yc managed-kubernetes cluster get-credentials --name netology-dev-k8s-cluster --external --force
+
--- 23-01/security_groups.tf
+++ 23-01/security_groups.tf
@@ -4,31 +4,30 @@ resource "yandex_vpc_security_group" "k8s_master_sg" {
   description = "Security group для Kubernetes Master"
   network_id  = yandex_vpc_network.k8s_network.id
 
-  # Правило 1: Доступ к API с вашего IP
+  # API доступ с вашего IP
   ingress {
     protocol       = "TCP"
     description    = "Kubernetes API (HTTPS)"
     from_port      = 443
     to_port        = 443
-    v4_cidr_blocks = var.allowed_ips
+    v4_cidr_blocks = ["95.24.9.243/32"]
   }
 
-  # Правило 2: Альтернативный порт API
   ingress {
     protocol       = "TCP"
     description    = "Kubernetes API (alternative)"
     from_port      = 6443
     to_port        = 6443
-    v4_cidr_blocks = var.allowed_ips
+    v4_cidr_blocks = ["95.24.9.243/32"]
   }
 
-  # Правило 3: Трафик от нод (через CIDR подсети)
+  # Весь трафик из подсети нод
   ingress {
-    protocol       = "TCP"
-    description    = "Traffic from nodes"
+    protocol       = "ANY"
+    description    = "All traffic from nodes subnet"
     from_port      = 0
     to_port        = 65535
-    v4_cidr_blocks = [yandex_vpc_subnet.k8s_subnet.v4_cidr_blocks[0]]
+    v4_cidr_blocks = ["10.10.0.0/16"]
   }
 
   # Исходящий трафик
@@ -36,7 +35,7 @@ resource "yandex_vpc_security_group" "k8s_master_sg" {
     protocol       = "ANY"
     description    = "Allow all outbound traffic"
     from_port      = 0
-    to_port        = 0
+    to_port        = 65535
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -47,31 +46,13 @@ resource "yandex_vpc_security_group" "k8s_nodes_sg" {
   description = "Security group для Kubernetes Nodes"
   network_id  = yandex_vpc_network.k8s_network.id
 
-  # Правило 1: Трафик между нодами
-  ingress {
-    protocol          = "ANY"
-    description       = "Traffic between nodes"
-    from_port         = 0
-    to_port           = 0
-    predefined_target = "self_security_group"
-  }
-
-  # Правило 2: Трафик от мастера (через CIDR подсети)
+  # Весь трафик из подсети мастера и нод
   ingress {
     protocol       = "ANY"
-    description    = "Traffic from master"
+    description    = "All traffic from cluster subnet"
     from_port      = 0
-    to_port        = 0
-    v4_cidr_blocks = [yandex_vpc_subnet.k8s_subnet.v4_cidr_blocks[0]]
-  }
-
-  # Правило 3: Доступ к kubelet от мастера
-  ingress {
-    protocol       = "TCP"
-    description    = "Kubelet from master"
-    from_port      = 10250
-    to_port        = 10250
-    v4_cidr_blocks = [yandex_vpc_subnet.k8s_subnet.v4_cidr_blocks[0]]
+    to_port        = 65535
+    v4_cidr_blocks = ["10.10.0.0/16"]
   }
 
   # Исходящий трафик
@@ -79,7 +60,7 @@ resource "yandex_vpc_security_group" "k8s_nodes_sg" {
     protocol       = "ANY"
     description    = "Allow all outbound traffic"
     from_port      = 0
-    to_port        = 0
+    to_port        = 65535
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
